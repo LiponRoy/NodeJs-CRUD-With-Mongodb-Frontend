4db7a29b95dfcdbf3335ea08538a9bd5829b78a2
Lipon Roy <liponbdbd@gmail.com>
2023-02-25 20:25:29 +0600

    project started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+	return (
+		<>
+			<div className=' h-screen w-full fcm'>
+				<span className=' text-4xl font-bold '>Lipon Roy Here...</span>
+				<span className=' text-2xl font-shantell'>Project Started...</span>
+			</div>
+		</>
+	);
+}
+
+export default App;
