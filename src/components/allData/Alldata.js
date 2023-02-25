d530eae4fdec794031a1e6852aab719057eb49f7
Lipon Roy <liponbdbd@gmail.com>
2023-02-25 22:57:49 +0600

    testing with some backend data using Axios

--- src/components/allData/Alldata.js
+++ src/components/allData/Alldata.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
+import Axios from 'axios';
 const Alldata = () => {
-	return <div className=' m-14'></div>;
+	// const baseURL = '/data/all';
+	const [post, setPost] = React.useState([]);
+	useEffect(() => {
+		Axios.get('/data/all').then((response) => {
+			setPost(response.data);
+		});
+	}, []);
+	return (
+		<div className=' m-14'>
+			<span className=' text-2xl'>Instractor data</span>
+			{post ? (
+				post.map((data) => (
+					<div key={data._id} className=' bg-slate-300 p-5 my-5 flex flex-col'>
+						<span>{data.name}</span>
+						<span>{data.email}</span>
+						<span>{data.address}</span>
+						<span>{data.status}</span>
+					</div>
+				))
+			) : (
+				<span> no data found</span>
+			)}
+		</div>
+	);
 };
 
 export default Alldata;
