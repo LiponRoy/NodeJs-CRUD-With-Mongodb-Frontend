0bd6865cb2811e9a2a95d21236850eebb2f0b0c5
Lipon Roy <liponbdbd@gmail.com>
2023-02-26 23:48:37 +0600

    redux toolkit setup successfully

--- src/components/allData/Data.js
+++ src/components/allData/Data.js
@@ -4,20 +4,20 @@ import { FaUserAlt, FaRegEnvelope, FaGlobe } from 'react-icons/fa';
 const Data = ({ id, name, email, address, status }) => {
 	return (
 		<div>
-			<div key={id} className=' bg-primary text-white p-5 my-5 flex flex-col rounded-md gap-1'>
+			<div key={id} className=' bg-gray-200 shadow-md text-gray-700  p-5 my-5 flex flex-col rounded-md gap-1'>
 				<FaUserAlt className=' mb-2' size={32}></FaUserAlt>
 				<span className=' text-2xl font-bold underline'>{name}</span>
 				<div className='flex items-center'>
 					<FaRegEnvelope className='mr-2' size={18}></FaRegEnvelope>
-					<span>{email}</span>
+					<span className=' text-xl '>{email}</span>
 				</div>
 				<div className='flex items-center'>
 					<FaGlobe className='mr-2' size={18}></FaGlobe>
-					<span>{address}</span>
+					<span className=' text-xl '>{address}</span>
 				</div>
 				<div className='flex items-center'>
 					<FaRegEnvelope className='mr-2' size={18}></FaRegEnvelope>
-					<span>{status}</span>
+					<span className=' text-xl '>{status}</span>
 				</div>
 
 				<div className=' flex items-center justify-between mt-2'>
